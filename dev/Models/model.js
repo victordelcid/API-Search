0e661fa6bcba22638eafe31620b7fba53aa7b87e
Víctor <victordelcid@ufm.edu>
2018-11-04 00:59:35 -0600

    Fixing redis client url

--- dev/Models/model.js
+++ dev/Models/model.js
@@ -1,6 +1,6 @@
 var redis = require("redis");
 
-let client = redis.createClient("redis-search.yermjr.ng.0001.use1.cache.amazonaws.com:6379");
+let client = redis.createClient("//redis-search.yermjr.ng.0001.use1.cache.amazonaws.com:6379");
 
 multi = client.multi();
 
