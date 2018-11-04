6c7a045087bf384bdb3b4371877ab0b489a31b6b
Víctor <victordelcid@ufm.edu>
2018-11-04 01:02:04 -0600

    Fixing redis client issues

--- dev/Models/model.js
+++ dev/Models/model.js
@@ -2,6 +2,6 @@ var redis = require("redis");
 
 let client = redis.createClient("//redis-search.yermjr.ng.0001.use1.cache.amazonaws.com:6379");
 
-multi = client.multi();
+//multi = client.multi();
 
-module.exports = multi;
+module.exports = client;
