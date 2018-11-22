96549eb45b4a10cfe8fb58c4f1806ae30e13e5c0
Víctor <victordelcid@ufm.edu>
2018-11-22 12:44:13 -0600

    Fixed routes

--- Search/dev/Routes/route.js
+++ Search/dev/Routes/route.js
@@ -2,8 +2,6 @@
 module.exports = function(app){
     var searchController = require('../Controllers/controller');
     
-    app.route('/search/')
-        .get(searchController.searchAPI);
     app.route('/history/')
         .get(searchController.historyAPI);
 };
